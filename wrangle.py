6b534f601955a7a1bb37ade1ed718d74970655eb
brandon <brandon.t.navarrete@gmail.com>
2023-04-06 19:41:18 -0500

    function added

--- wrangle.py
+++ wrangle.py
@@ -9,6 +9,10 @@ import time
 import json
 import re
 
+# Time and OS
+import time 
+import os 
+
 # Scraping imports
 import nltk
 from requests import get
@@ -97,5 +101,82 @@ def all_teams_2021():
     
         
     return df
+
+#------------------------------------------------------------------------------------------------------------
+
+def stats_df():
+    """
+    A function that reads robot statistics from a list of links to web pages,
+    filters and formats them to fit into a Pandas DataFrame, and concatenates all the resulting
+    DataFrames into one master DataFrame.
+    This uses a file named 'all_season_robot_links.json'
+
+    Returns:
+    --------
+    Pandas DataFrame:
+        A DataFrame containing robot statistics from all the links in the provided JSON file.
+    """
+    
+    # read in the JSON file containing the robot web page links
+    with open('all_season_robot_links.json') as json_file:
+        data = json.load(json_file)
+
+    # create an empty DataFrame to store concatenated tables
+    all_tables = pd.DataFrame()
+
+    # loop through every key in data
+    for key in data.keys():
+
+        print(f"Scraping statistics for {key} robots...")
+
+        # loop through every link associated with the current key
+        for i in data[key]:
+
+            print(f"\tScraping {i}...")
+
+            # try to read tables from the current link
+            try:
+                tables = pd.read_html(i)
+
+            # if no tables are found, skip to the next link
+            except ValueError:
+                continue
+
+            # try to format the first table in the resulting list of tables
+            try:
+                # select and transpose the relevant rows from the table
+                table = tables[0][['Stats', 'Career']].T
+
+                # use the first row as column names
+                table.columns = table.iloc[0]
+
+                # drop the now redundant row of column names
+                table = table.drop(index='Stats')
+
+                # reset the row index to start from 0
+                table = table.reset_index(drop=True)
+
+                # extract the robot name from the current link and add it as a column to the table
+                url = f'{i}'
+                match = re.search(r'/([^/]+)/?$', url)
+                if match:
+                    table['robot'] = match.group(1)
+
+            # if any errors occur while formatting the table, skip to the next link
+            except KeyError:
+                continue
+
+            # add the current table to the master DataFrame
+            all_tables = pd.concat([all_tables, table], ignore_index=True, axis=0)
+            print(f"\t\t...table added to master DataFrame ({all_tables.shape[0]} rows)")
+
+            # pause briefly to avoid overloading the web server with requests
+            time.sleep(1)
+
+    print("...done!")
+    # return the completed master DataFrame
+    # save to csv
+    all_tables.to_csv('all-table1.csv'')
+    return all_tables
 
 #------------------------------------------------------------------------------------------------------------
