45d7e3c560ca3f3d384b07ef32ad8055008a0003
Manuel Parra <manuel.parra1@gmail.com>
2023-04-11 10:16:25 -0500

    prepare

--- Manny/prepare.py
+++ Manny/prepare.py
@@ -28,3 +28,32 @@ def merge_matchup_history(stats_df, match_history_df):
     
     # Return the merged dataframe.
     return joined_df
+
+def clean_merged_robot_data(df):
+    """
+    Cleans a merged dataframe of robot statistics by removing unnecessary characters from the 'Value' column,
+    renaming the 'Stats' column to include the unit of measurement, and converting 'Value' to floats where appropriate.
+    
+    Args:
+    - df: Pandas dataframe of merged robot statistics
+    
+    Returns:
+    - df: cleaned Pandas dataframe of merged robot statistics
+    """
+    
+    # Create a boolean mask of rows where the 'Value' column ends with '%'
+    mask_percent = df['Value'].str.endswith('%').fillna(False).astype(bool)
+    
+    # Check if the 'Value' column contains the percent symbol, if so, remove it and convert to float
+    df.loc[mask_percent, 'Value'] = df[mask_percent]['Value'].str.rstrip('%').astype(float)/100
+    
+    # Create a boolean mask of rows where the 'Value' column matches the pattern: [number] s
+    mask_seconds = df['Value'].str.match(r'^\d+ s$').fillna(False).astype(bool)
+    
+    # Check if the 'Value' column contains an 's', if so, remove it and convert to float
+    df.loc[mask_seconds, 'Value'] = df[mask_seconds]['Value'].str.rstrip('s').astype(float)
+    
+    # Rename the 'Stats' column from 'Average knockout time' to 'Average knockout time (seconds)'
+    df.loc[df['Stats'] == 'Average knockout time', 'Stats'] = 'Average knockout time (seconds)'
+    
+    return df
